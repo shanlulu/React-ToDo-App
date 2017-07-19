60ef3c975f2c185d2ea3a319e5984030958fe87d
Shan <slu5@wellesley.edu>
2017-07-19 14:42:27 -0700

    setup react

---
+++ reactApp/app.js
@@ -0,0 +1,6 @@
+// var React = require('react');
+import React from 'react';
+import ReactDOM from 'react-dom';
+// var ReactDOM = require('react-dom');
+
+ReactDOM.render(<p>Resfreshing all the time</p>, document.getElementById('root'));
