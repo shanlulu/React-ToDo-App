a6931887845605a2e9e17c50bb356354807ad716
Shan <slu5@wellesley.edu>
2017-07-19 15:13:43 -0700

    add input line

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,18 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // var ReactDOM = require('react-dom');
 
-var dummyData = ['Learn Quantopian', 'Review React', 'Read \'The Crowd\'', 'Buy Plane Ticket', 'Send Dad My Emergency Contact']
-
-
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {dummyData.map(todo => <Todo task={todo}/>)}
-      </ul>
-    )
-  }
-}
+var dummyData = ['Learn Quantopian', 'Review React', 'Read \'The Crowd\'', 'Buy Plane Ticket', 'Send Dad My Emergency Contact'];
 
 class Todo extends React.Component {
   render() {
@@ -28,4 +17,39 @@ class Todo extends React.Component {
     )
   }
 }
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {dummyData.map(todo => <Todo task={todo}/>)}
+      </ul>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="task" value="" />
+        <button>
+          Add todo
+        </button>
+      </div>
+    )
+  }
+}
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <InputLine />
+        <TodoList />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
