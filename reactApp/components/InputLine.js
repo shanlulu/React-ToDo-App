d81ed5d13161f1569e7927ead9eb0addf3960152
Shan <slu5@wellesley.edu>
2017-07-19 16:44:45 -0700

    implement inputting todo

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 class InputLine extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      typedText: ''
+    }
+  };
+
+  handleTyping(event) {
+    event.preventDefault();
+    this.setState({ typedText: event.target.value});
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.submit(this.state.typedText);
+    this.setState({ typedText: '' });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="task" value="" />
-        <button onClick={() => {this.props.submit('Test Task')}}>
+        <input type="text" placeholder="task" value={this.state.typedText} onChange={(e) => this.handleTyping(e)}/>
+        <button onClick={(e) => {this.handleSubmit(e)}}>
           Add todo
         </button>
       </div>
